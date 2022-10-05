e4a55d806f5a490ff81f8bb83e35c2317acd5646
Vlad Filenko <yaadyk@gmail.com>
2022-10-05 19:07:14 +0400

    comit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// const pageWidth = document.documentElement.scrollWidth;
+// if(pageWidth < 768){
+// new Swiper('.swiper-container',{
+//     slidesPerView: 'auto',
+//     spaceBetween: 16,
+//     slidesOffsetBefore:16,
+//     slidesOffsetAfter:16,
+
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+//     });
+// } else{
+// };
+
+
+
+const slider = document.querySelector('.swiper-container');
+function mobileSlider() {
+	if (window.innerWidth < 768  && slider.dataset.mobile == 'false') {
+		mySwiper = new Swiper(slider, {
+		slidesPerView: 'auto',
+        spaceBetween: 16,
+        slidesOffsetBefore:16,
+        slidesOffsetAfter:16,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+			},
+		});
+
+		slider.dataset.mobile = 'true';
+	}
+
+	if (window.innerWidth > 768) {
+		slider.dataset.mobile = 'false';
+		if (slider.classList.contains('swiper-initialized')) {
+			mySwiper.destroy();
+		}
+	}
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider();
+});
+
+let brandsSwiper = document.querySelector('.brands__swiper');
+let showAll = document.querySelector('.show-all__link');
+let arry = document.querySelector('.show-all__link:before');
+showAll.addEventListener('click', function(evt){
+    evt.preventDefault();
+    brandsSwiper.classList.toggle('brands__swiper-show-all');
+    showAll.classList.toggle('show-all__link-rotate');
+
+    if(showAll.textContent === 'Показать все'){
+        showAll.textContent = 'Скрыть';
+    } else {
+        showAll.textContent = 'Показать все';
+}
+
+});
+
+
+
